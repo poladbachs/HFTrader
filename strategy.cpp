9a1a34ccee7cb400c99f23de2949ee9b8d70e763
Polad <bakhip@usi.ch>
2024-12-02 00:39:07 +0100

    method to process market data in cpp

--- strategy.cpp
+++ strategy.cpp
@@ -4,3 +4,14 @@
 // Constructor
 Strategy::Strategy(double stop_loss, double stop_win)
     : stop_loss(stop_loss), stop_win(stop_win), in_trade(false) {}
+
+// Method to process market data
+void Strategy::onMarketData(const MarketData& md) {
+    std::cout << "Received market data: " << md.price << std::endl;
+    if (md.symbol == "EUR/USD") {
+        data.push_back(md);
+        if (data.size() >= 3) {
+            execute();
+        }
+    }
+}
