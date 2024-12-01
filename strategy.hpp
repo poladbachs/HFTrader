83f34125376ca55f45abf118a5914fb970be28d4
Polad <bakhip@usi.ch>
2024-12-02 00:19:15 +0100

    processing incoming market data for EUR/USD

--- strategy.hpp
+++ strategy.hpp
@@ -16,6 +16,16 @@ class Strategy {
     Strategy(double stop_loss, double stop_win)
     : stop_loss(stop_loss), stop_win(stop_win), in_trade(false) {}
 
+    void onMarketData(const MarketData& md) {
+        std::cout << "Received market data: " << md.price << std::endl;
+        if (md.symbol == "EUR/USD") {
+            data.push_back(md);
+            if (data.size() >= 3) {
+                execute();
+            }
+        }
+    }
+
 };
 
 #endif
