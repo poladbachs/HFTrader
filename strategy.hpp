a4ce3903ebcfc1024eb3d104f01ab2f03853302b
Polad <bakhip@usi.ch>
2024-12-02 00:29:21 +0100

    rearranged which are private and public, fields and helper func are private, rest is public

--- strategy.hpp
+++ strategy.hpp
@@ -7,12 +7,24 @@
 #include "market_data.hpp"
 
 class Strategy {
+private:
     std::vector<MarketData> data;
     double stop_loss;
     double stop_win;
     double entry_price;
     bool in_trade;
+    bool isLocalMinimum() {
+        bool is_minimum = data[data.size() - 2].price < data[data.size() - 3].price &&
+                          data[data.size() - 2].price < data.back().price;
 
+        std::cout << "Checking for local minimum at " << data[data.size() - 2].price
+                  << " (previos: " << data[data.size() - 3].price
+                  << ", next: " << data.back().price
+                  << ") - is minimum: " << is_minimum << std::endl;
+        return is_minimum;
+    }
+
+public:
     Strategy(double stop_loss, double stop_win)
     : stop_loss(stop_loss), stop_win(stop_win), in_trade(false) {}
 
@@ -26,16 +38,6 @@ class Strategy {
         }
     }
     
-    bool isLocalMinimum() {
-        bool is_minimum = data[data.size() - 2].price < data[data.size() - 3].price &&
-                          data[data.size() - 2].price < data.back().price;
-
-        std::cout << "Checking for local minimum at " << data[data.size() - 2].price
-                  << " (previos: " << data[data.size() - 3].price
-                  << ", next: " << data.back().price
-                  << ") - is minimum: " << is_minimum << std::endl;
-        return is_minimum;
-    }
 
     void execute() {
         std::cout << "Executing strategy..." << std::endl;
